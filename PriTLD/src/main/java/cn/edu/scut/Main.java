afc8b8b323e87c993d36f9256cfc12d253fb6ea2
Aroc <85719360+liukanshan1@users.noreply.github.com>
2023-01-26 22:24:36 +0800

    添加核心算法依赖

--- PriTLD/src/main/java/cn/edu/scut/Main.java
+++ PriTLD/src/main/java/cn/edu/scut/Main.java
@@ -1,7 +1,10 @@
 package cn.edu.scut;
 
+import Priloc.area.basic.Circle;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Circle s;
     }
 }
